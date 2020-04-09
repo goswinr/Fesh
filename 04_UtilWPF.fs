f600178e818c2ac53802ca237276e81ee65b51f6
Goswin <gwinsky@yahoo.com>
2020-04-09 12:59:19 +0200

    util  INotifyPropertyChanged

--- 04_UtilWPF.fs
+++ 04_UtilWPF.fs
@@ -10,6 +10,40 @@ open System.Windows.Media
 module UtilWPF =    
 
 
+    open System.ComponentModel
+    open Microsoft.FSharp.Quotations
+    open Microsoft.FSharp.Quotations.Patterns
+
+    type ViewModelBase() = //http://www.fssnip.net/4Q/title/F-Quotations-with-INotifyPropertyChanged
+        let propertyChanged = new Event<_, _>()
+        let toPropName(query : Expr) = 
+            match query with
+            | PropertyGet(a, b, list) ->
+                b.Name
+            | _ -> ""
+
+        interface INotifyPropertyChanged with
+            [<CLIEvent>]
+            member x.PropertyChanged = propertyChanged.Publish
+
+        abstract member OnPropertyChanged: string -> unit
+        default x.OnPropertyChanged(propertyName : string) =
+            propertyChanged.Trigger(x, new PropertyChangedEventArgs(propertyName))
+
+        member x.OnPropertyChanged(expr : Expr) =
+            let propName = toPropName(expr)
+            x.OnPropertyChanged(propName)
+
+    type TestModel() =
+        inherit ViewModelBase()
+
+        let mutable selectedItem : obj = null
+
+        member x.SelectedItem
+            with get() = selectedItem
+            and set(v : obj) = 
+                selectedItem <- v
+                x.OnPropertyChanged(<@ x.SelectedItem @>)
     
 
     //see: http://www.fssnip.net/4W/title/Calculator
@@ -119,3 +153,6 @@ module UtilWPF =
     
     ///Removes bytes from each color channel to increase darkness
     let darker  (amount:int) (br:SolidColorBrush)  = changeLuminace -amount br
+
+
+
