c2c040fd2af6b8a5dc87ff9751ee3240ef058ea9
Goswin <gwinsky@yahoo.com>
2020-05-20 11:59:46 +0200

    add App before

--- App.fs
+++ App.fs
@@ -10,15 +10,18 @@ module App =
     /// hostName: a string for the name of the hosting App (will be used for settings file name an displayed in the Title Bar.
     /// Call seff.Window.Show() on the returned Seff object.    
     /// Use seff.Fsi.OnStarted and seff.Fsi.OnIsReady Events to implement undo and redo in host App.
-    let runEditorHosted (mainWindowHandle, hostName) : Seff =
+    let runEditorHosted (mainWindowHandle, hostName) : Application*Seff =
+        let app = Application()
         let seff = Initialize.everything (Hosted hostName , [| |])        
-        Interop.WindowInteropHelper(seff.Window).Owner <- mainWindowHandle
+        if mainWindowHandle <> IntPtr.Zero then 
+            Interop.WindowInteropHelper(seff.Window).Owner <- mainWindowHandle
         //win.Show() // do in host instead, so that the host can control the window show time
-        seff
+        app,seff
 
 
     [< EntryPoint >]
     [< STAThread >] 
     let runEditorStandalone args : int =   
+        let app = Application()
         let seff = Initialize.everything (Standalone,args)
-        (new Application()).Run(seff.Window) 
+        app.Run(seff.Window) 
