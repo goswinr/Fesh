60e05fdb4e2eb55b6053fccd088389a45d86face
Goswin <gwinsky@yahoo.com>
2020-09-03 13:09:05 +0200

    Statusbar big refactoring

--- Src/Views/StatusBar.fs
+++ Src/Views/StatusBar.fs
@@ -16,75 +16,39 @@ open Seff.Config
 open Seff.Editor
 
 
-
-type FsiOutputQuiet (grid:TabsAndLog) as this = 
-    inherit TextBlock()
-    do     
-        this.Padding <- Thickness(6. , 2. , 6., 2. ) //left ,top, right, bottom)
-        this.Text <- "FSI output is on"
-        this.ToolTip <- "Click here to enabel or disable the default output from fsi in the log window"
-        this.MouseLeftButtonDown.Add ( fun a -> 
-            if grid.Config.Settings.GetBool Settings.keyFsiQuiet false then 
-                this.Text <- "FSI prints to log window"
-                grid.Config.Settings.SetBool Settings.keyFsiQuiet false
-                grid.Tabs.Fsi.Initalize()
-            else
-                this.Text <- "FSI is quiet"
-                grid.Config.Settings.SetBool Settings.keyFsiQuiet true
-                grid.Tabs.Fsi.Initalize()
-            )  
-            
-
-
-
-        
-
-type StatusBar (grid:TabsAndLog, cmds:Commands)  = // TODO better make it dependent on commands , not fsi
-
-    let log = grid.Log
-    let tabs= grid.Tabs
-    let config = grid.Config 
-    let fsi = tabs.Fsi
-
-    let bar = new Primitives.StatusBar() 
-   
-    
-
+module private StatusbarStyle = 
     let padding = Thickness(6. , 2. , 6., 2. ) //left ,top, right, bottom)
 
+type CheckerStatus (grid:TabsAndLog) as this = 
+    inherit TextBlock()
     
-    let checker = Checker.GetOrCreate(config)
-     
-    let fsiState = TextBlock(Text="FSI is initializing ...", Padding = padding, ContextMenu = makeContextMenu [ menuItem cmds.CancelFSI ])
-    
+    let tabs = grid.Tabs
     let checkingTxt = "checking for Errors ..."
-
-    let compilerErrors = new TextBlock(Text=checkingTxt , Padding = padding) 
-    
-    let originalBackGround = compilerErrors.Background
-            
+    let checker = Checker.GetOrCreate(grid.Config)
+    let originalBackGround = this.Background
+                
     let updateCheckState(iEditor:IEditor)= 
         //log.PrintDebugMsg "Setting errors for %A %A " iEditor.FileInfo iEditor.CheckRes.Value.checkRes.Errors.Length 
         match iEditor.CheckState with
         | Done res ->                                            
                 let es = res.checkRes.Errors
                 if es.Length = 0 then 
-                    compilerErrors.Text <- "No compiler errors"
-                    compilerErrors.Background <- Brushes.Green |> brighter 120
-                    compilerErrors.ToolTip <- "FSarp Compiler Service found no Errors in " + tabs.Current.FormatedFileName
+                    this.Text <- "No compiler errors"
+                    this.Background <- Brushes.Green |> brighter 120
+                    this.ToolTip <- "FSarp Compiler Service found no Errors in " + tabs.Current.FormatedFileName
                 else 
                     let ers = es|> Seq.filter (fun e -> e.Severity = FSharpErrorSeverity.Error) |> Seq.length
                     let was = es.Length - ers
                     if ers = 0 then 
-                        compilerErrors.Text <- sprintf "Compiler warnings: %d" was
-                        compilerErrors.Background <- Brushes.Yellow   |> brighter 40  
+                        this.Text <- sprintf "Compiler warnings: %d" was
+                        this.Background <- Brushes.Yellow   |> brighter 40  
                     elif was = 0 then
-                        compilerErrors.Text <- sprintf "Compiler errors: %d" ers
-                        compilerErrors.Background <- Brushes.Red   |> brighter 150  
+                        this.Text <- sprintf "Compiler errors: %d" ers
+                        this.Background <- Brushes.Red   |> brighter 150  
                     else
-                        compilerErrors.Text <- sprintf "Compiler errors: %d, warnings: %d" ers was
-                        compilerErrors.Background <- Brushes.Red   |> brighter 150             
-                    compilerErrors.ToolTip <- makePanelVert [                         
+                        this.Text <- sprintf "Compiler errors: %d, warnings: %d" ers was
+                        this.Background <- Brushes.Red   |> brighter 150             
+                    this.ToolTip <- makePanelVert [                         
                         if ers>0 then TextBlock(Text="Errors:", FontSize = 14. , FontWeight = FontWeights.Bold )
                         for e in es|> Seq.filter (fun e -> e.Severity = FSharpErrorSeverity.Error)  do new TextBlock(Text = sprintf "• Line %d: %s" e.StartLineAlternate e.Message)
                         if was>0 then TextBlock(Text="Warnings:", FontSize = 14. , FontWeight = FontWeights.Bold )
@@ -99,73 +63,97 @@ type StatusBar (grid:TabsAndLog, cmds:Commands)  = // TODO better make it depend
                     match iEditor.CheckState with
                     | Running id300 ->
                         if id300 = id0 then // this is still the most recent checker
-                            compilerErrors.Text <- checkingTxt
-                            compilerErrors.Background <- originalBackGround 
+                            this.Text <- checkingTxt
+                            this.Background <- originalBackGround 
                     | Done _ | NotStarted | Failed -> ()
             } |> Async.StartImmediate 
         
         | NotStarted -> // these below never happen because event is only triggerd on success
-            compilerErrors.Text <- "Initializing compiler.."
-            compilerErrors.Background <- originalBackGround 
+            this.Text <- "Initializing compiler.."
+            this.Background <- originalBackGround 
         
         | Failed -> // these below never happen because event is only triggerd on success
-            compilerErrors.Text <- "Fs Checker failed to complete."
-            compilerErrors.Background <- Brushes.Magenta 
-
+            this.Text <- "Fs Checker failed to complete."
+            this.Background <- Brushes.Magenta 
     
 
+    do     
+        this.Padding <-StatusbarStyle.padding
 
-    do        
-        
-        fsi.OnStarted.Add(fun code -> 
-            fsiState.Background <- Brushes.Orange   |> brighter 20 
-            match code.file with 
-            |SetTo fi -> 
-                if code.allOfFile then fsiState.Text <- sprintf "FSI is running %s  ..." fi.Name
-                else                   fsiState.Text <- sprintf "FSI is running selected code from file %s  ..." fi.Name
-            |NotSet ->                 fsiState.Text <- "FSI is running ..." )
-
-        fsi.OnIsReady.Add(fun _ -> 
-            fsiState.Text <- "FSI is ready" 
-            fsiState.Background <- Brushes.Green |> brighter 120)
+        this.Text <- checkingTxt
         
         tabs.OnTabChanged.Add (fun t -> updateCheckState(t.Editor))
             
         checker.OnChecked.Add updateCheckState
 
         checker.OnChecking.Add updateCheckState
+ 
+type FsiRunStatus (grid:TabsAndLog, cmds:Commands) as this = 
+    inherit TextBlock()
+    do     
+        this.Padding <- StatusbarStyle.padding
+        this.Text <- "FSI is initializing ..."
+        this.ContextMenu <- makeContextMenu [ menuItem cmds.CancelFSI ]
+        //this.ToolTip <- "Click here to enabel or disable the default output from fsi in the log window"
+              
+        grid.Tabs.Fsi.OnStarted.Add(fun code -> 
+            this.Background <- Brushes.Orange   |> brighter 20 
+            match code.file with 
+            |SetTo fi -> 
+                if code.allOfFile then this.Text <- sprintf "FSI is running %s  ..." fi.Name
+                else                   this.Text <- sprintf "FSI is running selected code from file %s  ..." fi.Name
+            |NotSet ->                 this.Text <- "FSI is running ..." )
 
-        bar.Items.Add compilerErrors          |> ignore 
-        bar.Items.Add (new Separator())       |> ignore 
-        bar.Items.Add fsiState                |> ignore 
-        bar.Items.Add (new Separator())       |> ignore 
-        bar.Items.Add (new FsiOutputQuiet(grid)) |> ignore 
-        bar.Items.Add (new Separator())       |> ignore 
+        grid.Tabs.Fsi.OnIsReady.Add(fun _ -> 
+            this.Text <- "FSI is ready" 
+            this.Background <- Brushes.Green |> brighter 120)
+
+type FsiOutputStatus (grid:TabsAndLog) as this = 
+    inherit TextBlock()
+    do     
+        this.Padding <- StatusbarStyle.padding
+        this.Text <- "FSI output is on"
+        this.ToolTip <- "Click here to enabel or disable the default output from fsi in the log window"
+        this.MouseLeftButtonDown.Add ( fun a -> 
+            if grid.Config.Settings.GetBool Settings.keyFsiQuiet false then 
+                this.Text <- "FSI prints to log window"
+                grid.Config.Settings.SetBool Settings.keyFsiQuiet false
+                grid.Tabs.Fsi.Initalize()
+            else
+                this.Text <- "FSI is quiet"
+                grid.Config.Settings.SetBool Settings.keyFsiQuiet true
+                grid.Tabs.Fsi.Initalize()
+            )  
+            
+type AsyncStatus (grid:TabsAndLog) as this = 
+    inherit TextBlock()
+    let fsi = grid.Tabs.Fsi
+    let set = grid.Config.Settings
+    let sync = "FSI evaluation mode: Synchronos" 
+    let asyn = "FSI evaluation mode: Asynchronos"
         
-        //------- add Async or Sync state-------------
-        match config.HostingInfo.Mode with
-        |Standalone -> 
-            bar.Items.Add (new StatusBarItem())   |> ignore // to fill remaining space
-        |Hosted _ ->
-            let ini =   
-                match config.Settings.GetBool "asyncFsi" true  with
-                | false  ->  "FSI evaluation mode: Synchronos" 
-                | true ->  "FSI evaluation mode: Asynchronos"
-        
-            let asyncDesc = new TextBlock( Text =  ini  , Padding = padding) 
-            bar.Items.Add( new StatusBarItem(Content=asyncDesc)) |> ignore
-            bar.Items.Add( new Separator())  |> ignore 
-            bar.Items.Add( new StatusBarItem())   |> ignore // to fill remaining space
-            asyncDesc.ToolTip <- "Click to switch between synchronous and asynchronous evaluation in FSI,\r\nsynchronous is needed for UI interaction,\r\nasynchronous allows easy cancellation and keeps the editor window alive"
-            asyncDesc.MouseDown.Add(fun _ -> fsi.ToggleSync()) //done in fsi module
-      
-            fsi.OnModeChanged.Add(function 
-                | Sync  -> asyncDesc.Text <- "FSI evaluation mode: Synchronos" 
-                | Async -> asyncDesc.Text <- "FSI evaluation mode: Asynchronos"  )
+    do     
+        this.Padding <- StatusbarStyle.padding
+        this.Text <- if set.GetBool "asyncFsi" true  then asyn else sync
+        this.ToolTip <- "Click to switch between synchronous and asynchronous evaluation in FSI,\r\nsynchronous is needed for UI interaction,\r\nasynchronous allows easy cancellation and keeps the editor window alive"
+        this.MouseDown.Add(fun _ -> fsi.ToggleSync()) //done in fsi module      
+        fsi.OnModeChanged.Add(function 
+            | Sync  -> this.Text <- sync 
+            | Async -> this.Text <- asyn  )
+ 
+type StatusBar (grid:TabsAndLog, cmds:Commands)  = // TODO better make it dependent on commands , not fsi
 
+    let bar = new Primitives.StatusBar()   
 
-
-
+    do 
+        bar.Items.Add (new CheckerStatus(grid))                                         |> ignore 
+        bar.Items.Add (new Separator())                                                 |> ignore 
+        bar.Items.Add (new FsiRunStatus (grid, cmds))                                   |> ignore 
+        bar.Items.Add (new Separator())                                                 |> ignore 
+        bar.Items.Add (new FsiOutputStatus(grid))                                       |> ignore 
+        bar.Items.Add (new Separator())                                                 |> ignore 
+        if grid.Config.HostingInfo.IsHosted then bar.Items.Add( new AsyncStatus(grid))  |> ignore
+        bar.Items.Add( new StatusBarItem())                                             |> ignore // to fill remaining space
 
     member this.Bar =  bar
   
