a9c18ba8cd9d9a9efd2eb1fc7f1c288e66cd51ea
Goswin <gwinsky@yahoo.com>
2020-10-12 17:24:04 +0200

    config.FoldingStatus

--- Src/Config/Config.fs
+++ Src/Config/Config.fs
@@ -7,15 +7,25 @@ open Seff
 
 type Config (log:ISeffLog, context:HostingMode, startupArgs:string[]) =
     
-    let hostInfo = HostingInfo(context)
+    let  hostInfo                   = HostingInfo                 (context)
+    let  settings                   = Settings                    (log, hostInfo)
+    let  recentlyUsedFiles          = RecentlyUsedFiles           (log, hostInfo)
+    let  openTabs                   = OpenTabs                    (log, hostInfo, startupArgs)
+    let  defaultCode                = DefaultCode                 (log, hostInfo)
+    let  autoCompleteStatistic      = AutoCompleteStatistic       (log, hostInfo)
+    let  assemblyReferenceStatistic = AssemblyReferenceStatistic  (log, hostInfo)
+    let  fsiArugments               = FsiArugments                (log, hostInfo)
+    let  foldingStatus              = FoldingStatus               (log, hostInfo, recentlyUsedFiles)
+ 
 
-    member val Settings                   = Settings                    (log, hostInfo)
-    member val RecentlyUsedFiles          = RecentlyUsedFiles           (log, hostInfo)
-    member val OpenTabs                   = OpenTabs                    (log, hostInfo, startupArgs)
-    member val DefaultCode                = DefaultCode                 (log, hostInfo)
-    member val AutoCompleteStatistic      = AutoCompleteStatistic       (log, hostInfo)
-    member val AssemblyReferenceStatistic = AssemblyReferenceStatistic  (log, hostInfo)
-    member val FsiArugments               = FsiArugments                (log, hostInfo)
-    member val HostingInfo                = hostInfo      
-    member val Log                        = log 
-
+    member this.HostingInfo                = hostInfo      
+    member this.Log                        = log 
+    member this.Settings                   = settings                  
+    member this.RecentlyUsedFiles          = recentlyUsedFiles         
+    member this.OpenTabs                   = openTabs                  
+    member this.DefaultCode                = defaultCode               
+    member this.AutoCompleteStatistic      = autoCompleteStatistic     
+    member this.AssemblyReferenceStatistic = assemblyReferenceStatistic
+    member this.FsiArugments               = fsiArugments  
+    member this.FoldingStatus              = foldingStatus  
+    
---
+++ Src/Config/FoldingStatus.fs
@@ -0,0 +1,69 @@
+﻿namespace Seff.Config
+
+open Seff
+open System
+open System.Text
+open System.Collections.Generic
+open System.IO
+open ICSharpCode.AvalonEdit.Folding
+   
+/// A class to hold the statistic of most used toplevel auto completions
+type FoldingStatus (log:ISeffLog, hostInfo:HostingInfo,recentlyUsedFiles:RecentlyUsedFiles) =
+    let writer = SaveWriter(log)
+        
+    let  sep = '|' // separator 
+    
+    let filePath = hostInfo.GetPathToSaveAppData("FoldingStatus.txt")
+    
+    let mutable waitingForFileRead =  true
+
+    let foldingStatus = 
+        let dict=Dictionary<string,bool []>() 
+        async{
+            try            
+                if IO.File.Exists filePath then 
+                    for ln in  IO.File.ReadAllLines filePath do
+                    match ln.Split(sep) with
+                    | [|k;v|] -> 
+                        //log.PrintDebugMsg "%s for %s" v k
+                        let vs = Seq.map ( fun c -> if c = '0' then false elif c = '1' then true else failwithf "bad char %c in FoldingStatus" c) v |> Array.ofSeq
+                        dict.[k] <- vs 
+                    | _ -> log.PrintAppErrorMsg "Bad line in FoldingStatus file : '%s'" ln                   
+            with e -> 
+                log.PrintAppErrorMsg "Error load FoldingStatus: %A"   e
+            } |> Async.Start 
+        waitingForFileRead <- false
+        dict
+
+    let foldingStatusAsString () = 
+        let sb = StringBuilder() 
+        for KeyValue(k,v) in foldingStatus do
+            if recentlyUsedFiles.Contains(k)then 
+                let vs= v |> Seq.map ( fun b -> if b then "1" else "0") |> String.concat ""
+                sb.Append(k).Append(sep).AppendLine(vs) |> ignore
+        sb.ToString() 
+    
+    member this.WaitingForFileRead = waitingForFileRead
+
+    member this.Get(ed:IEditor) =
+        match ed.FilePath with
+        | NotSet -> [| |]
+        | SetTo fi -> 
+            match foldingStatus.TryGetValue fi.FullName with
+            |true,vs -> vs
+            |_      -> 
+                match foldingStatus.TryGetValue fi.Name with
+                |true,vs -> vs
+                |_      -> [| |]    
+
+    member this.Set(ed:IEditor, folds:FoldingManager) =
+        match ed.FilePath with
+        | NotSet -> ()
+        | SetTo fi -> 
+            let vs = [| for f in folds.AllFoldings do f.IsFolded |]
+            foldingStatus.[fi.FullName] <- vs
+            foldingStatus.[fi.Name] <- vs
+            writer.WriteDelayed (filePath, foldingStatusAsString, 500)
+        
+    member this.Save() =
+        writer.WriteDelayed (filePath, foldingStatusAsString, 500)
