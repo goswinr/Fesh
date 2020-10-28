df79436ee8d53fcdfca435447cbc0188b39232f9
Goswin <gwinsky@yahoo.com>
2020-10-28 14:45:33 +0100

    bump to FCS37

--- Src/Editor/CodeCompletion.fs
+++ Src/Editor/CodeCompletion.fs
@@ -152,8 +152,8 @@ type Completions(avaEdit:TextEditor,config:Config, checker:Checker, errorHighlig
     /// retuns "loading" text and triggers async computation to get and update with actual text 
     member this.GetToolTip(it:FSharpDeclarationListItem)= 
         async{
-            let raw = it.StructuredDescriptionText
-            //let! raw = it.StructuredDescriptionTextAsync
+            //let raw = it.StructuredDescriptionText //FCS 33.0.1
+            let! raw = it.StructuredDescriptionTextAsync //FCS 37.0.0
             let structured = 
                 if optArgsDict.ContainsKey it.FullName then  TypeInfo.getFormated (raw, optArgsDict.[it.FullName])
                 else                                         TypeInfo.getFormated (raw, ResizeArray(0))
