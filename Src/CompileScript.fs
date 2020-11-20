fc5de1aa3524b7f9c3cee0ce3d0e02a5255fa484
Goswin <gwinsky@yahoo.com>
2020-11-20 14:52:34 +0100

    Update CompileScript.fs
    add xm and pdb in lib

--- Src/CompileScript.fs
+++ Src/CompileScript.fs
@@ -10,6 +10,7 @@ open Seff.Model
 open Seff.Config
 open Seff.Util
 open System.Windows.Media
+open System.Drawing
 
 
 
@@ -63,8 +64,9 @@ module CompileScript =
 
     let libFolderName = "lib"
     
-    let getRefs(code:string, libFolder:string)=
-        seq{ for ln in code.Split('\n') do 
+    let getRefs(code:string, libFolder:string, log:ISeffLog)=
+        let refs = ResizeArray()
+        for ln in code.Split('\n') do 
                 let tln = ln.Trim()
                 if tln.StartsWith "#r " then 
                     let _,path,_ = String.splitTwice "\""  "\"" tln
@@ -72,18 +74,32 @@ module CompileScript =
                     let nameDll = p.Split('/') |> Seq.last 
                     let name = nameDll |> replace ".dll" ""
                     if p.Contains "/" || p.Contains "\\" then
-                        if p.Contains "/RhinoCommon.dll" then   name, path, false
+                        if p.Contains "/RhinoCommon.dll" then   refs.Add(name, path, false)
                         else 
                             if libFolder <>"" then 
                                 let newp = IO.Path.Combine(libFolder,nameDll)
-                                if IO.File.Exists newp then IO.File.Delete newp
-                                IO.File.Copy(path,newp )
-                                name, libFolderName+"/"+nameDll, true
+                                try
+                                    if IO.File.Exists newp then IO.File.Delete newp
+                                    IO.File.Copy(path,newp )
+
+                                    let xml  = Path.ChangeExtension(path,"xml")
+                                    let xmln = Path.ChangeExtension(newp,"xml")
+                                    if IO.File.Exists xmln then IO.File.Delete xmln
+                                    if IO.File.Exists (xml) then IO.File.Copy(xml, xmln)
+
+                                    let pdb  = Path.ChangeExtension(path,"pdb")
+                                    let pdbn = Path.ChangeExtension(newp,"pdb")
+                                    if IO.File.Exists pdbn then IO.File.Delete pdbn
+                                    if IO.File.Exists (pdb) then IO.File.Copy(pdb, pdbn)
+
+                                with e ->
+                                    log.PrintIOErrorMsg "Error in getting refrences: %A" e
+                                refs.Add(name, libFolderName+"/"+nameDll, true)
                             else
-                                name, path, true
+                                refs.Add(name, path, true)
                     else 
-                        name, "", true // for BCL dlls of the  .Net framework
-                    }
+                        refs.Add(name, "", true) // for BCL dlls of the  .Net framework
+        refs
                 
     let mutable version = "0.0.0.1"
 
@@ -91,51 +107,55 @@ module CompileScript =
         match fp with 
         | NotSet -> log.PrintAppErrorMsg "Cannot compile an unsaved script save it first"
         |SetTo fi ->
-            let version = "0.0.0.1"
-            let name = fi.Name.Replace(".fsx","")
-            let nameSpace = name |> toCamelCase |> up1
-            let projFolder = IO.Path.Combine(fi.DirectoryName,name) 
-            let libFolder = if copyDlls then IO.Path.Combine(projFolder,libFolderName) else "" 
-            if libFolder<>"" then  IO.Directory.CreateDirectory(libFolder)  |> ignore 
-            IO.Directory.CreateDirectory(projFolder)  |> ignore            
-            let fsProj = IO.Path.Combine(projFolder,nameSpace + ".fsproj")
-            let fsxName = nameSpace + ".fsx"
-            let fsxPath = IO.Path.Combine(projFolder,fsxName)
-            IO.File.WriteAllText(fsxPath,code)
-            let refs = 
-                getRefs (code ,libFolder)
-                |> Seq.map getRefXml
-                |> String.concat Environment.NewLine
-            baseXml
-            |> replace "        " "" //cler white space at beginning of lines
-            |> replace "rootNamespace" nameSpace
-            |> replace "assemblyName" nameSpace
-            |> replace "9.7.8.6.5.1" version
-            |> replace "9.7.8.6.5.2" version
-            |> replace "9.7.8.6.5.3" version
-            |> replace "<!--references-->" refs
-            |> replace "pathToFsx" fsxName
-            |> fun s -> 
-                IO.File.WriteAllText(fsProj,s,Text.Encoding.UTF8)
-                log.PrintInfoMsg "project created at %s\r\nnow compiling ..." fsProj
-                //https://stackoverflow.com/questions/1145969/processinfo-and-redirectstandardoutput
-                let p = new System.Diagnostics.Process()
-                p.EnableRaisingEvents <- true
-                p.StartInfo.FileName <- "dotnet"
-                let fsProjinQuotes = "\"" + fsProj + "\"" 
-                p.StartInfo.Arguments <- String.concat " " ["build"; fsProjinQuotes;  "--configuration Release"]
-                p.StartInfo.UseShellExecute <- false
-                p.StartInfo.CreateNoWindow <- true //true if the process should be started without creating a new window to contain it
-                p.StartInfo.RedirectStandardError <-true
-                p.StartInfo.RedirectStandardOutput <-true
-                p.OutputDataReceived.Add ( fun d -> log.PrintCustomBrush Media.Brushes.DarkOliveGreen   "%s" d.Data)
-                p.ErrorDataReceived.Add (  fun d -> log.PrintAppErrorMsg "%s" d.Data)               
-                p.Exited.Add( fun _ -> log.PrintInfoMsg  "Compiling done")
-                p.Start() |> ignore
-                p.BeginOutputReadLine()
-                p.BeginErrorReadLine()
-                //log.PrintInfoMsg "compiling to %s" (IO.Path.Combine(projFolder,"bin","Release","netstandard2.0",nameSpace+".dll")) 
-                p.WaitForExit()
+            async{
+                log.PrintInfoMsg "compiling %s ..." fi.Name
+                let version = "0.0.0.1"
+                let name = fi.Name.Replace(".fsx","")
+                let nameSpace = name |> toCamelCase |> up1
+                let projFolder = IO.Path.Combine(fi.DirectoryName,name) 
+                let libFolder = if copyDlls then IO.Path.Combine(projFolder,libFolderName) else "" 
+                if libFolder<>"" then  IO.Directory.CreateDirectory(libFolder)  |> ignore 
+                IO.Directory.CreateDirectory(projFolder)  |> ignore            
+                let fsProj = IO.Path.Combine(projFolder,nameSpace + ".fsproj")
+                let fsxName = nameSpace + ".fsx"
+                let fsxPath = IO.Path.Combine(projFolder,fsxName)
+                IO.File.WriteAllText(fsxPath,code)
+                let refs = 
+                    getRefs (code ,libFolder,log)
+                    |> Seq.map getRefXml
+                    |> String.concat Environment.NewLine
+                baseXml
+                |> replace "        " "" //cler white space at beginning of lines
+                |> replace "rootNamespace" nameSpace
+                |> replace "assemblyName" nameSpace
+                |> replace "9.7.8.6.5.1" version
+                |> replace "9.7.8.6.5.2" version
+                |> replace "9.7.8.6.5.3" version
+                |> replace "<!--references-->" refs
+                |> replace "pathToFsx" fsxName
+                |> fun s -> 
+                    IO.File.WriteAllText(fsProj,s,Text.Encoding.UTF8)
+                    log.PrintInfoMsg "project created at %s\r\nstarting dotnet build ..." fsProj
+                    //https://stackoverflow.com/questions/1145969/processinfo-and-redirectstandardoutput
+                    let p = new System.Diagnostics.Process()
+                    p.EnableRaisingEvents <- true
+                    p.StartInfo.FileName <- "dotnet"
+                    let fsProjinQuotes = "\"" + fsProj + "\"" 
+                    p.StartInfo.Arguments <- String.concat " " ["build"; fsProjinQuotes;  "--configuration Release"]
+                    log.PrintCustomBrush Media.Brushes.DarkBlue "%s %s" p.StartInfo.FileName p.StartInfo.Arguments
+                    p.StartInfo.UseShellExecute <- false
+                    p.StartInfo.CreateNoWindow <- true //true if the process should be started without creating a new window to contain it
+                    p.StartInfo.RedirectStandardError <-true
+                    p.StartInfo.RedirectStandardOutput <-true
+                    p.OutputDataReceived.Add ( fun d -> log.PrintCustomBrush Media.Brushes.DarkOliveGreen   "%s" d.Data)
+                    p.ErrorDataReceived.Add (  fun d -> log.PrintAppErrorMsg "%s" d.Data)               
+                    p.Exited.Add( fun _ -> log.PrintInfoMsg  "Build finnished!")
+                    p.Start() |> ignore
+                    p.BeginOutputReadLine()
+                    p.BeginErrorReadLine()
+                    //log.PrintInfoMsg "compiling to %s" (IO.Path.Combine(projFolder,"bin","Release","netstandard2.0",nameSpace+".dll")) 
+                    p.WaitForExit()
+                    } |> Async.Start
 
 
            
