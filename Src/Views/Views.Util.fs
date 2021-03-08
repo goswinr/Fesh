1631e577c06b697bb713b7835cd406cc6bb01193
Goswin <gwinsky@yahoo.com>
2021-03-08 14:26:39 +0100

    FsWpf

--- Src/Views/Views.Util.fs
+++ Src/Views/Views.Util.fs
@@ -172,6 +172,36 @@ module Util =
                 }
 
 (*
+// TODO   unused ??!!
+module FsWpf = // from https://github.com/tatsuya-midorikawa/FsWpf/blob/main/src/templates/FsWpf.ViewModels/MainWindowViewModel.fs
+
+    open System.ComponentModel
+    open System.Runtime.CompilerServices
+    open System.Runtime.InteropServices
+
+    [<AbstractClass>]
+    type ViewModel() =
+      let propertyChanged = Event<_, _>()
+  
+      interface INotifyPropertyChanged with
+        [<CLIEvent>]
+        member __.PropertyChanged = propertyChanged.Publish
+
+      member __.OnPropertyChanged([<CallerMemberName; Optional; DefaultParameterValue("")>] memberName: string) =
+        if not(System.String.IsNullOrEmpty(memberName)) then
+          propertyChanged.Trigger(__, PropertyChangedEventArgs(memberName))
+
+    type MainWindowViewModel() =
+      inherit ViewModel()
+      let mutable title = "F# Wpf Sample Text"
+  
+      member __.Title
+        with get() = title
+        and set(title') = title <- title'; __.OnPropertyChanged()
+
+    
+    /// alternative
+
     open System.ComponentModel
     
     type ViewModelBase() = //http://www.fssnip.net/4Q/title/F-Quotations-with-INotifyPropertyChanged
