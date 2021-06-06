8635c255922b5c9ea5feb7dbd4aa8aec2473a644
Goswin <gwinsky@yahoo.com>
2021-06-06 10:41:05 +0200

    fix window state if max on second screen

--- Src/Views/Window.fs
+++ Src/Views/Window.fs
@@ -15,6 +15,8 @@ type Window (config:Config)=
 
     let log = config.Log
 
+    let mutable setMaxAfterLoading = false
+
     let mutable isMinOrMax = false 
     
     let mutable wasMax = false //indicating if the Window was in  Fullscreen mode before switching to temporary Log only fullscreeen
@@ -38,47 +40,52 @@ type Window (config:Config)=
               
         //-------------------------------------------------------------------------
         // -  all below code is for load and safe window location and size ---
-        //-------------------------------------------------------------------------
+        //-------------------------------------------------------------------------        
+        
+        
+        win.WindowStartupLocation <- WindowStartupLocation.Manual
+        let winTop    = config.Settings.GetFloat "WindowTop"    0.0
+        let winLeft   = config.Settings.GetFloat "WindowLeft"   0.0 
+        let winHeight = config.Settings.GetFloat "WindowHeight" 800.0
+        let winWidth  = config.Settings.GetFloat "WindowWidth"  800.0
+
+        //let maxW = float <| Array.sumBy (fun (sc:Forms.Screen) -> sc.WorkingArea.Width)  Forms.Screen.AllScreens  // neded for dual screens ?, needs wins.forms
+        //let maxH = float <| Array.sumBy (fun (sc:Forms.Screen) -> sc.WorkingArea.Height) Forms.Screen.AllScreens //https://stackoverflow.com/questions/37927011/in-wpf-how-to-shift-a-win-onto-the-screen-if-it-is-off-the-screen/37927012#37927012
+            
+        let offTolerance = 25.0 // beeing 20 pixel off screen is still good enough for beeing on screen and beeing draggable
+
+        let maxW = SystemParameters.VirtualScreenWidth   + offTolerance
+        let maxH = SystemParameters.VirtualScreenHeight  + offTolerance // somehow a window docked on the right is 7 pix bigger than the screen ?? // TODO check dual screens !!
+            
+        win.Top <-     winTop 
+        win.Left <-    winLeft 
+        win.Height <-  winHeight
+        win.Width <-   winWidth
         
         if config.Settings.GetBool "WindowIsMax" false then
-            win.WindowState <- WindowState.Maximized
+            //win.WindowState <- WindowState.Maximized // always put is on first screen, do in loaded event instead
+            setMaxAfterLoading <- true
             isMinOrMax  <- true
             wasMax <- true
-        else
-            win.WindowStartupLocation <- WindowStartupLocation.Manual
-            let winTop    = config.Settings.GetFloat "WindowTop"    0.0
-            let winLeft   = config.Settings.GetFloat "WindowLeft"   0.0 
-            let winHeight = config.Settings.GetFloat "WindowHeight" 800.0
-            let winWidth  = config.Settings.GetFloat "WindowWidth"  800.0
+        
+        elif  winTop  < -offTolerance || winHeight + winTop  > maxH then 
+            log.PrintfnAppErrorMsg "Could not restore previous Editor Window position:"
+            log.PrintfnAppErrorMsg "winTopPosition: %.1f  + winHeight: %.1f  = %.1f that is bigger than maxH: %.1f + %.1f tolerance" winTop winHeight   ( winHeight + winTop ) SystemParameters.VirtualScreenHeight offTolerance
+            win.WindowStartupLocation <- WindowStartupLocation.CenterScreen                
+            win.Height <- 600.0                
+            win.Width  <- 600.0
 
-            //let maxW = float <| Array.sumBy (fun (sc:Forms.Screen) -> sc.WorkingArea.Width)  Forms.Screen.AllScreens  // neded for dual screens ?, needs wins.forms
-            //let maxH = float <| Array.sumBy (fun (sc:Forms.Screen) -> sc.WorkingArea.Height) Forms.Screen.AllScreens //https://stackoverflow.com/questions/37927011/in-wpf-how-to-shift-a-win-onto-the-screen-if-it-is-off-the-screen/37927012#37927012
-            
-            let offTolerance = 25.0 // beeing 20 pixel off screen is still good enough for beeing on screen and beeing draggable
-
-            let maxW = SystemParameters.VirtualScreenWidth   + offTolerance
-            let maxH = SystemParameters.VirtualScreenHeight  + offTolerance // somehow a window docked on the right is 7 pix bigger than the screen ?? // TODO check dual screens !!
-            
-            win.Top <-     winTop 
-            win.Left <-    winLeft 
-            win.Height <-  winHeight
-            win.Width <-   winWidth
-
-            if  winTop  < -offTolerance || winHeight + winTop  > maxH then 
-                log.PrintfnAppErrorMsg "Could not restore previous Editor Window position:"
-                log.PrintfnAppErrorMsg "winTopPosition: %.1f  + winHeight: %.1f  = %.1f that is bigger than maxH: %.1f + %.1f tolerance" winTop winHeight   ( winHeight + winTop ) SystemParameters.VirtualScreenHeight offTolerance
-                win.WindowStartupLocation <- WindowStartupLocation.CenterScreen                
-                win.Height <- 600.0                
-                win.Width  <- 600.0
-
-            if winLeft < -offTolerance || winWidth  + winLeft > maxW then
-                log.PrintfnAppErrorMsg "Could not restore previous Editor Window position:"
-                log.PrintfnAppErrorMsg "winLeftPosition: %.1f  + winWidth: %.1f = %.1f that is bigger than maxW: %.1f + %.1f tolerance" winLeft winWidth ( winWidth +  winLeft) SystemParameters.VirtualScreenWidth offTolerance
-                win.WindowStartupLocation <- WindowStartupLocation.CenterScreen
-                win.Height <- 600.0                
-                win.Width  <- 600.0
-
-
+        elif winLeft < -offTolerance || winWidth  + winLeft > maxW then
+            log.PrintfnAppErrorMsg "Could not restore previous Editor Window position:"
+            log.PrintfnAppErrorMsg "winLeftPosition: %.1f  + winWidth: %.1f = %.1f that is bigger than maxW: %.1f + %.1f tolerance" winLeft winWidth ( winWidth +  winLeft) SystemParameters.VirtualScreenWidth offTolerance
+            win.WindowStartupLocation <- WindowStartupLocation.CenterScreen
+            win.Height <- 600.0                
+            win.Width  <- 600.0
+        
+        //Turns out that we cannot maximize the window until it's loaded. 
+        //http://mostlytech.blogspot.com/2008/01/maximizing-wpf-window-to-second-monitor.html
+        win.Loaded.Add (fun _ -> if setMaxAfterLoading then win.WindowState <- WindowState.Maximized)
+        
         win.LocationChanged.Add(fun e -> // occures for every pixel moved
             async{
                 // normally the state change event comes after the location change event but before size changed. async sleep in LocationChanged prevents this
@@ -88,7 +95,7 @@ type Window (config:Config)=
                         config.Settings.SetFloatDelayed "WindowTop"  win.Top  89 // get float in statchange maximised needs to access this before 350 ms pass
                         config.Settings.SetFloatDelayed "WindowLeft" win.Left 95
                         config.Settings.Save ()
-                        //log.PrintfnDebugMsg  "%s Location Changed: Top=%.0f Left=%.0f State=%A" Time.nowStrMilli win.Top win.Left win.WindowState
+                        //log.PrintfnDebugMsg  "Location Changed: Top=%.0f Left=%.0f State=%A" win.Top win.Left win.WindowState
                 }
                 |> Async.StartImmediate
             )
@@ -103,31 +110,31 @@ type Window (config:Config)=
                 config.Settings.SetBool  "WindowIsMax" false
                 isMinOrMax <- false
                 config.Settings.Save ()
-                //log.PrintfnDebugMsg "Normal: %s State changed=%A Top=%.0f Left=%.0f Width=%.0f Height=%.0f" Time.nowStrMilli win.WindowState win.Top win.Left win.ActualWidth win.ActualHeight 
+                //log.PrintfnDebugMsg "Normal: State changed=%A Top=%.0f Left=%.0f Width=%.0f Height=%.0f" win.WindowState win.Top win.Left win.ActualWidth win.ActualHeight 
 
             | WindowState.Maximized ->
                 // normally the state change event comes after the location change event but before size changed. async sleep in LocationChanged prevents this
                 isMinOrMax  <- true
                 config.Settings.SetBool  "WindowIsMax" true
                 config.Settings.Save  ()    
-                //log.PrintfnDebugMsg "Maximised: %s State changed=%A Top=%.0f Left=%.0f Width=%.0f Height=%.0f" Time.nowStrMilli win.WindowState win.Top win.Left win.ActualWidth win.ActualHeight 
+                //log.PrintfnDebugMsg "Maximised: State changed=%A Top=%.0f Left=%.0f Width=%.0f Height=%.0f" win.WindowState win.Top win.Left win.ActualWidth win.ActualHeight 
                           
 
             |WindowState.Minimized ->                 
                 isMinOrMax  <- true
-                //log.PrintfnDebugMsg "Minimised: %s State changed=%A Top=%.0f Left=%.0f Width=%.0f Height=%.0f" Time.nowStrMilli win.WindowState win.Top win.Left win.ActualWidth win.ActualHeight 
+                //log.PrintfnDebugMsg "Minimised: State changed=%A Top=%.0f Left=%.0f Width=%.0f Height=%.0f"  win.WindowState win.Top win.Left win.ActualWidth win.ActualHeight 
                
             |wch -> 
                 log.PrintfnAppErrorMsg "unknown WindowState State change=%A" wch
                 isMinOrMax  <- true
             )
 
-        win.SizeChanged.Add (fun e ->
+        win.SizeChanged.Add (fun e -> // does no get trigger on maximising 
             if win.WindowState = WindowState.Normal &&  not isMinOrMax  then 
                 config.Settings.SetFloatDelayed "WindowHeight" win.Height 89
                 config.Settings.SetFloatDelayed "WindowWidth"  win.Width  95
                 config.Settings.Save ()
-                //log.dlog (sprintf "%s Size Changed: Width=%.0f Height=%.0f State=%A" Time.nowStrMilli win.Width win.Height win.WindowState)
+                //log.PrintfnInfoMsg "Size Changed: State=%A Top=%.0f Left=%.0f Width=%.0f Height=%.0f" win.WindowState win.Top win.Left win.ActualWidth win.ActualHeight
             )
         
        
