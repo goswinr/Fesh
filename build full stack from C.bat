3a3d03e9f97998a3e973cd255c59369afe7c6923
Goswin <gwinsky@yahoo.com>
2021-06-27 09:36:37 +0200

    update build script

--- build full stack from C.bat
+++ build full stack from C.bat
@@ -24,26 +24,15 @@ rmdir /s /q  "C:/GitHub/Rhino.Scripting/binRh7"
 dotnet build "C:/GitHub/Rhino.Scripting/Rhino.Scripting7.fsproj" -p:Platform=x64 --configuration Release
 
 
-
-
-
-@REM :: Rhino7Full to include FsEx and Rhino.Scripting
-@REM rmdir /s /q "C:\GitHub\Seff.Rhino\binRh7Full"
-@REM dotnet build "C:\GitHub\Seff.Rhino\Seff.Rhino7.Full.fsproj" 
-
-
 :: Rhino6
 :: first delete obj folder so that ther reference really goes to rhino 6 and not 7
 rmdir /s /q  "C:/GitHub/Seff.Rhino/obj" 
 rmdir /s /q  "C:/GitHub/Seff.Rhino/binRh6"
-dotnet build "C:/GitHub/Seff.Rhino/Seff.Rhino6.fsproj" --configuration Release
-@REM rmdir /s /q  "C:/GitHub/Rhino.Scripting/obj"
-@REM rmdir /s /q  "C:/GitHub/Rhino.Scripting/binRh6"
-@REM dotnet build "C:/GitHub/Rhino.Scripting/Rhino.Scripting6.fsproj" --configuration Release
+dotnet build "C:/GitHub/Seff.Rhino/Seff.Rhino6.fsproj" -p:Platform=x64 --configuration Release
 
-@REM :: Rhino6Full to include FsEx and Rhino.Scripting
-@REM rmdir /s /q  "C:\GitHub\Seff.Rhino\binRh6Full"
-@REM dotnet build "C:\GitHub\Seff.Rhino\Seff.Rhino6.Full.fsproj" --configuration Release
+rmdir /s /q  "C:/GitHub/Rhino.Scripting/obj"
+rmdir /s /q  "C:/GitHub/Rhino.Scripting/binRh6"
+dotnet build "C:/GitHub/Rhino.Scripting/Rhino.Scripting6.fsproj" -p:Platform=x64 --configuration Release
 
 
 @REM :: Revit 2018
@@ -55,9 +44,5 @@ dotnet build "C:/GitHub/Seff.Rhino/Seff.Rhino6.fsproj" --configuration Release
 @REM dotnet build "C:/GitHub/Seff.Revit/Seff.Revit2018.fsproj" --configuration Release
 
 
-@REM :: and again to have a distributable stabndalone version that does not show erroes even if a ref to rhino is there
-@REM :: to include FsEx and Rhino.Scripting
-@REM rmdir /s /q  "C:/GitHub/Seff/binStandaloneFsEx"
-@REM dotnet build "C:/GitHub/Seff/SeffStandaloneFsEx.fsproj" --configuration Release
 
 PAUSE
