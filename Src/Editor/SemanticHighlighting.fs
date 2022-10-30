674d8b1562b9836e8d240a51f03bad3a5157aac9
Goswin Rothenthal <gwinsky@yahoo.com>
2022-10-30 22:51:10 +0100

    fix semantic caching

--- Src/Editor/SemanticHighlighting.fs
+++ Src/Editor/SemanticHighlighting.fs
@@ -55,7 +55,7 @@ module SemColor =
     let TypeDef                      = Brushes.Thistle |> darker 50 |> freeze
     let Plaintext                    = Brushes.OrangeRed |> darker 60 |> freeze   
     
-    let UnUsed                       = Brushes.LightGray |> freeze   
+    let UnUsed                       = Brushes.Gray |> freeze   
 
 module SemAction = 
     open SemColor
@@ -108,7 +108,8 @@ type Sc = SemanticClassificationType
 type SemanticColorizier (ied:TextEditor, edId:Guid, ch:Checker) = 
     inherit Rendering.DocumentColorizingTransformer()
     
-    let mutable lastRun = 0L
+    let mutable lastCheckId = -1L
+    let mutable lastCode = ""
 
     let mutable allRanges = [||]
 
@@ -119,35 +120,26 @@ type SemanticColorizier (ied:TextEditor, edId:Guid, ch:Checker) =
         async{            
             let! us = UnusedDeclarations.getUnusedDeclarations(chRes.checkRes,true)
             for u in us do unusedDecl.Add u
-            }   |> Async.RunSynchronously
+            }  
+        |> Async.RunSynchronously
     
     // skip semantic highlighting for these, covered in xshd:
-    let skipFunc(st:int, en:int, res:CheckResults)=        
-        match res.code with 
-        |PartialCode _ -> false // should never happen
-        |FullCode s -> 
-            let w = s.[st..en]
-            w.StartsWith    "failwith"
-            || w.StartsWith "failIfFalse" // from FsEx
-            || w.StartsWith "print"
-            || w.StartsWith "eprint"
+    let skipFunc(st:int, en:int)=        
+        let w = lastCode.[st..en]
+        w.StartsWith    "failwith"
+        || w.StartsWith "failIfFalse" // from FsEx
+        || w.StartsWith "print"
+        || w.StartsWith "eprint"
 
-    let skipModul(st:int, en:int, res:CheckResults)=        
-        match res.code with 
-        |PartialCode _ -> false // should never happen
-        |FullCode s -> 
-            let w = s.[st..en]
-            w.StartsWith "Printf"
+    let skipModul(st:int, en:int)=        
+        let w = lastCode.[st..en]
+        w.StartsWith "Printf"
     
     /// becaus some thims the range of a property starts before the point
-    let correctStart(st:int, en:int, res:CheckResults) =        
-        match res.code with 
-        |PartialCode _ -> st // should never happen
-        |FullCode s -> 
-            let w = s.[st..en]
-            match s.IndexOf('.',st,en-st) with 
-            | -1 -> st 
-            |  i -> i + 1
+    let correctStart(st:int, en:int) =        
+        match lastCode.IndexOf('.',st,en-st) with 
+        | -1 -> st 
+        |  i -> i + 1
 
 
     let action (el:VisualLineElement,brush:SolidColorBrush,r:Text.Range) =
@@ -158,78 +150,86 @@ type SemanticColorizier (ied:TextEditor, edId:Guid, ch:Checker) =
 
     /// This gets called for every visible line on any view change
     override this.ColorizeLine(line:Document.DocumentLine) =
-            match ch.GlobalCheckState with 
-            |Checking _
-            |GettingCode _
-            |NotStarted 
-            |Failed -> ()
-            |Done chRes ->
-                if chRes.editorId = edId then                     
-                    if lastRun <> chRes.checkId then 
-                        allRanges <- chRes.checkRes.GetSemanticClassification(None)
-                        setUnusedDecl(chRes)
-                        lastRun <-chRes.checkId                         
+        match ch.GlobalCheckState with 
+        |Checking _
+        |GettingCode _
+        |NotStarted 
+        |Failed -> ()
+        |Done chRes ->
+            if chRes.editorId = edId then                     
+                if lastCheckId <> chRes.checkId then // to make sure this update only happens on the firts line 
+                    lastCheckId <- chRes.checkId  
+                    allRanges <- chRes.checkRes.GetSemanticClassification(None)
+                    setUnusedDecl(chRes)
+                    match chRes.code with 
+                    |PartialCode _ -> () // should never happen
+                    |FullCode    s -> lastCode <- s
                     
-                    let lineNo = line.LineNumber
-                    let off = line.Offset    
+        let lineNo = line.LineNumber
+        let offSt = line.Offset    
+        let offEn = offSt + line.Length    
                     
-                    // TODO use binary search instead !!
-                    for i=allRanges.Length-1 downto 0 do // doing a reverse search solves a highlighting problem where ranges overlap
-                        let sem = allRanges.[i]
-                        let r = sem.Range
-                        if r.StartLine = lineNo && r.EndLine = lineNo then 
-                            let st = off + sem.Range.StartColumn
-                            let en = off + sem.Range.EndColumn
-                            //try // beacuse other wise Seff crashes with  AppDomain.CurrentDomain.UnhandledException on bad ranges
-                            match sem.Type with 
-                            | Sc.ReferenceType               -> base.ChangeLinePart(st,en, SemAction.ReferenceType              )
-                            | Sc.ValueType                   -> base.ChangeLinePart(st,en, SemAction.ValueType                  )
-                            | Sc.UnionCase                   -> base.ChangeLinePart(st,en, SemAction.UnionCase                  )
-                            | Sc.UnionCaseField              -> base.ChangeLinePart(st,en, SemAction.UnionCaseField             )
-                            | Sc.Function                    -> if not(skipFunc(st,en,chRes)) then base.ChangeLinePart(st,en, SemAction.Function)
-                            | Sc.Property                    -> base.ChangeLinePart(correctStart(st,en,chRes),en, SemAction.Property                   )
-                            | Sc.MutableVar                  -> base.ChangeLinePart(st,en, SemAction.MutableVar                 )
-                            | Sc.Module                      -> if not(skipModul(st,en,chRes)) then base.ChangeLinePart(st,en, SemAction.Module                     )
-                            | Sc.Namespace                   -> base.ChangeLinePart(st,en, SemAction.Namespace                  )
-                            //| Sc.Printf                      -> base.ChangeLinePart(st,en, SemAction.Printf                     )
-                            | Sc.ComputationExpression       -> base.ChangeLinePart(st,en, SemAction.ComputationExpression      )
-                            | Sc.IntrinsicFunction           -> base.ChangeLinePart(st,en, SemAction.IntrinsicFunction          )
-                            | Sc.Enumeration                 -> base.ChangeLinePart(st,en, SemAction.Enumeration                )
-                            | Sc.Interface                   -> base.ChangeLinePart(st,en, SemAction.Interface                  )
-                            | Sc.TypeArgument                -> base.ChangeLinePart(st,en, SemAction.TypeArgument               )
-                            | Sc.Operator                    -> base.ChangeLinePart(st,en, SemAction.Operator                   )
-                            | Sc.DisposableType              -> base.ChangeLinePart(st,en, SemAction.DisposableType             )
-                            | Sc.DisposableTopLevelValue     -> base.ChangeLinePart(st,en, SemAction.DisposableTopLevelValue    )
-                            | Sc.DisposableLocalValue        -> base.ChangeLinePart(st,en, SemAction.DisposableLocalValue       )
-                            | Sc.Method                      -> base.ChangeLinePart(st,en, SemAction.Method                     )
-                            | Sc.ExtensionMethod             -> base.ChangeLinePart(st,en, SemAction.ExtensionMethod            )
-                            | Sc.ConstructorForReferenceType -> base.ChangeLinePart(st,en, SemAction.ConstructorForReferenceType)
-                            | Sc.ConstructorForValueType     -> base.ChangeLinePart(st,en, SemAction.ConstructorForValueType    )
-                            | Sc.Literal                     -> base.ChangeLinePart(st,en, SemAction.Literal                    )
-                            | Sc.RecordField                 -> base.ChangeLinePart(st,en, SemAction.RecordField                )
-                            | Sc.MutableRecordField          -> base.ChangeLinePart(st,en, SemAction.MutableRecordField         )
-                            | Sc.RecordFieldAsFunction       -> base.ChangeLinePart(st,en, SemAction.RecordFieldAsFunction      )
-                            | Sc.Exception                   -> base.ChangeLinePart(st,en, SemAction.Exception                  )
-                            | Sc.Field                       -> base.ChangeLinePart(st,en, SemAction.Field                      )
-                            | Sc.Event                       -> base.ChangeLinePart(st,en, SemAction.Event                      )
-                            | Sc.Delegate                    -> base.ChangeLinePart(st,en, SemAction.Delegate                   )
-                            | Sc.NamedArgument               -> base.ChangeLinePart(st,en, SemAction.NamedArgument              )
-                            | Sc.Value                       -> base.ChangeLinePart(st,en, SemAction.Value                      )
-                            | Sc.LocalValue                  -> base.ChangeLinePart(st,en, SemAction.LocalValue                 )
-                            | Sc.Type                        -> base.ChangeLinePart(st,en, SemAction.Type                       )
-                            | Sc.TypeDef                     -> base.ChangeLinePart(st,en, SemAction.TypeDef                    )
-                            | Sc.Plaintext                   -> base.ChangeLinePart(st,en, SemAction.Plaintext                  )   
-                            | _ -> ()
-                            //with e -> 
-                            //    ISeffLog.printError $"sem.Type {sem.Type}:\r\n on Line {line} for {r}"
-                            //    ISeffLog.printError $"off {off}, st:{st}, en:{en}"
-                            //    ISeffLog.printError $"MSG:{e}" 
+        // TODO use binary search instead !!
+        for i=allRanges.Length-1 downto 0 do // doing a reverse search solves a highlighting problem where ranges overlap
+            let sem = allRanges.[i]
+            let r = sem.Range
+            if r.StartLine = lineNo && r.EndLine = lineNo then 
+                let st = offSt + sem.Range.StartColumn
+                let en = offSt + sem.Range.EndColumn
+                // saftey check since we are reusing old ranges untill new check results are available:
+                if en >  offSt  && en <= offEn && st >= offSt  && st <  offEn && en < lastCode.Length then 
+                    //try // beacuse other wise Seff crashes with  AppDomain.CurrentDomain.UnhandledException on bad ranges
+                    match sem.Type with 
+                    | Sc.ReferenceType               -> base.ChangeLinePart(st,en, SemAction.ReferenceType              )
+                    | Sc.ValueType                   -> base.ChangeLinePart(st,en, SemAction.ValueType                  )
+                    | Sc.UnionCase                   -> base.ChangeLinePart(st,en, SemAction.UnionCase                  )
+                    | Sc.UnionCaseField              -> base.ChangeLinePart(st,en, SemAction.UnionCaseField             )
+                    | Sc.Function                    -> if not(skipFunc(st,en)) then base.ChangeLinePart(st,en, SemAction.Function)
+                    | Sc.Property                    -> base.ChangeLinePart(correctStart(st,en),en, SemAction.Property                   )
+                    | Sc.MutableVar                  -> base.ChangeLinePart(st,en, SemAction.MutableVar                 )
+                    | Sc.Module                      -> if not(skipModul(st,en)) then base.ChangeLinePart(st,en, SemAction.Module                     )
+                    | Sc.Namespace                   -> base.ChangeLinePart(st,en, SemAction.Namespace                  )
+                    //| Sc.Printf                      -> base.ChangeLinePart(st,en, SemAction.Printf                     ) // covered in xshd file 
+                    | Sc.ComputationExpression       -> base.ChangeLinePart(st,en, SemAction.ComputationExpression      )
+                    | Sc.IntrinsicFunction           -> base.ChangeLinePart(st,en, SemAction.IntrinsicFunction          )
+                    | Sc.Enumeration                 -> base.ChangeLinePart(st,en, SemAction.Enumeration                )
+                    | Sc.Interface                   -> base.ChangeLinePart(st,en, SemAction.Interface                  )
+                    | Sc.TypeArgument                -> base.ChangeLinePart(st,en, SemAction.TypeArgument               )
+                    | Sc.Operator                    -> base.ChangeLinePart(st,en, SemAction.Operator                   )
+                    | Sc.DisposableType              -> base.ChangeLinePart(st,en, SemAction.DisposableType             )
+                    | Sc.DisposableTopLevelValue     -> base.ChangeLinePart(st,en, SemAction.DisposableTopLevelValue    )
+                    | Sc.DisposableLocalValue        -> base.ChangeLinePart(st,en, SemAction.DisposableLocalValue       )
+                    | Sc.Method                      -> base.ChangeLinePart(st,en, SemAction.Method                     )
+                    | Sc.ExtensionMethod             -> base.ChangeLinePart(st,en, SemAction.ExtensionMethod            )
+                    | Sc.ConstructorForReferenceType -> base.ChangeLinePart(st,en, SemAction.ConstructorForReferenceType)
+                    | Sc.ConstructorForValueType     -> base.ChangeLinePart(st,en, SemAction.ConstructorForValueType    )
+                    | Sc.Literal                     -> base.ChangeLinePart(st,en, SemAction.Literal                    )
+                    | Sc.RecordField                 -> base.ChangeLinePart(st,en, SemAction.RecordField                )
+                    | Sc.MutableRecordField          -> base.ChangeLinePart(st,en, SemAction.MutableRecordField         )
+                    | Sc.RecordFieldAsFunction       -> base.ChangeLinePart(st,en, SemAction.RecordFieldAsFunction      )
+                    | Sc.Exception                   -> base.ChangeLinePart(st,en, SemAction.Exception                  )
+                    | Sc.Field                       -> base.ChangeLinePart(st,en, SemAction.Field                      )
+                    | Sc.Event                       -> base.ChangeLinePart(st,en, SemAction.Event                      )
+                    | Sc.Delegate                    -> base.ChangeLinePart(st,en, SemAction.Delegate                   )
+                    | Sc.NamedArgument               -> base.ChangeLinePart(st,en, SemAction.NamedArgument              )
+                    | Sc.Value                       -> base.ChangeLinePart(st,en, SemAction.Value                      )
+                    | Sc.LocalValue                  -> base.ChangeLinePart(st,en, SemAction.LocalValue                 )
+                    | Sc.Type                        -> base.ChangeLinePart(st,en, SemAction.Type                       )
+                    | Sc.TypeDef                     -> base.ChangeLinePart(st,en, SemAction.TypeDef                    )
+                    | Sc.Plaintext                   -> base.ChangeLinePart(st,en, SemAction.Plaintext                  )  
+                    | _ -> () 
+                    //with e -> 
+                    //    ISeffLog.printError  $"sem.type {sem.Type}:\r\n on line {line} for {r}"
+                    //    ISeffLog.printError  $"offSt {offSt} to offEn{offEn} for , st:{st} to en:{en}"
+                    //    ISeffLog.printError  $"MSG:{e}" 
                         
-                        for r in unusedDecl do                     
-                            if r.StartLine = lineNo && r.EndLine = lineNo then 
-                                let st = off + r.StartColumn
-                                let en = off + r.EndColumn
-                                base.ChangeLinePart(st,en, SemAction.UnUsed) 
+        for r in unusedDecl do                     
+            if r.StartLine = lineNo && r.EndLine = lineNo then 
+                let st = offSt + r.StartColumn
+                let en = offSt + r.EndColumn
+                if en >  offSt  && en <= offEn && st >= offSt  && st <  offEn && en < lastCode.Length then
+                    base.ChangeLinePart(st,en, SemAction.UnUsed) 
+                        
 
 module SemanticHighlighting =
 
