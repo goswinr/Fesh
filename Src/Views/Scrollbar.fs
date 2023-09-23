139c04e28152de81c756869e83e94386ae5e7156
Goswin Rothenthal <goswin@rothenthal.com>
2023-09-23 21:32:31 +0200

    scroll bar ok

--- Src/Views/Scrollbar.fs
+++ Src/Views/Scrollbar.fs
@@ -26,6 +26,7 @@ open Seff.Util.Str
 
 module MagicScrollbar =      
     open Seff.Util.General
+    open System
 
   
     // see // https://github.com/icsharpcode/SharpDevelop/blob/master/src/AddIns/DisplayBindings/AvalonEdit.AddIn/Src/EnhancedScrollBar.cs
@@ -33,63 +34,76 @@ module MagicScrollbar =
     type Marks = ResizeArray<int*SolidColorBrush>
 
     [<AllowNullLiteral>]
-    type ScrollbarAdorner(ed:TextEditor, errs:ErrorHighlighter, trackGrid:Grid)  as this =
-        inherit Adorner(trackGrid)
+    type ScrollbarAdorner(ed:TextEditor, state:InteractionState, errs:ErrorHighlighter, track: Track)  as this =        
+        inherit Adorner(track)
 
-        let mutable isActive = false
+        let textView = ed.TextArea.TextView
+
+        let mutable idOfMarks = 0L
+
+        let mutable isTrackShowing = false
 
         let markLineNos = ref (Marks())
 
         let setLineNos (v: Marks) = 
-            if not <| Util.General.areSameBy fst v markLineNos.Value then // compare by fst, that is only the line number ??
+            if not <| Util.General.areSameBy fst v markLineNos.Value then // compare by fst, that is only the line number
                 markLineNos.Value <- v
-                ed.Dispatcher.Invoke (fun _ -> this.InvalidateVisual()
-                )
-        
-        /// the size of the arrows at the top and bottom of the scrollbar
-        let scrollbarArrowsSize = 17. 
+                idOfMarks <- state.DocChangedId.Value
+                ed.Dispatcher.Invoke (fun _ -> this.InvalidateVisual())      
+
+        let visualTopCache = Array.create (ErrorUtil.maxErrorCountToTrack*4) 0.0
                 
         do             
-            //base.Cursor <- Cursors.Hand
+            //base.Cursor <- Cursors.Hand // https://github.com/icsharpcode/SharpDevelop/blob/master/src/AddIns/DisplayBindings/AvalonEdit.AddIn/Src/EnhancedScrollBar.cs
             //base.ToolTip <- "empty"
             
-            ed.TextArea.TextView.VisualLinesChanged.Add (fun _ -> if isActive then this.InvalidateVisual())   
-            errs.FoundErrors.Add (fun _ -> setLineNos errs.ErrorsLines.Value )
+            ed.TextArea.TextView.VisualLinesChanged.Add (fun _ -> if isTrackShowing then this.InvalidateVisual() )   
+            errs.FoundErrors.Add (fun _                        -> if isTrackShowing then setLineNos errs.ErrorsLines.Value )
         
-        member this.IsActive 
-            with get() = isActive 
-            and set(v) = isActive <- v
+        member this.IsTrackShowing 
+            with get() = isTrackShowing 
+            and set(v) = isTrackShowing <- v
 
         override this.OnRender(drawingContext : DrawingContext) =   
-            if isActive then                
-                let renderSize = base.RenderSize                
-                let textView = ed.TextArea.TextView
+            if isTrackShowing  then
+                //textView.EnsureVisualLines() // this is needed to get the correct visual line count
+                let renderSize = base.RenderSize
+                let lineHeight = textView.DefaultLineHeight
                 let documentHeight = textView.DocumentHeight
-                let lnNos = markLineNos.Value // this iteration never fails, even if the value in the ref gets replaced while looping
+                let lnNos = markLineNos.Value // this iteration never fails, even if the value in the ref gets replaced while looping                            
                 for i = 0 to lnNos.Count - 1 do 
                     let lnNo, brush = lnNos.[i]
+                    let visualTop =
+                        if state.IsLatest idOfMarks then 
+                            // GetVisualTopByDocumentLine fails with null ref exception if lnNo is bigger than document 
+                            // (lines where deleted but Marks still has the bigger count because checker has not updated yet)
+                            let vt = textView.GetVisualTopByDocumentLine (lnNo) 
+                            visualTopCache.[i] <- vt
+                            vt
+                        else
+                            visualTopCache.[i]
 
-                    let visualTop = textView.GetVisualTopByDocumentLine (lnNo) 
-                    let trackHeight = renderSize.Height - scrollbarArrowsSize * 2.
-                    let renderPos = ((visualTop / documentHeight) * trackHeight) + scrollbarArrowsSize
-                    //eprintfn $"renderPos: {renderPos} visualTop: {visualTop} documentHeight: {documentHeight} trackHeight: {trackHeight} renderSize.Width{renderSize.Width}"
-                    
-                    let x = 1. //3.
-                    let y = renderPos - 1.
-                    let width = renderSize.Width //- 6.
-                    let height = 2.
-                    let rect = new Rect(x, y, width, height)
-                    drawingContext.DrawRectangle (brush, null, rect)                          
+                    if visualTop  < documentHeight then   // so that markers are not drawn below the bottom of the scroll track
+                        let visualMiddle = visualTop + lineHeight * 0.5      // *0.5 to get text middle              
+                        let trackHeight = renderSize.Height 
+                        let renderPos = ((visualMiddle / documentHeight) * trackHeight) 
+                        let boxHeight = max 1. ((lineHeight / documentHeight) * trackHeight) 
+                        
+                        let x = 0. //3.
+                        let y = renderPos - boxHeight * 0.5 
+                        let width = renderSize.Width //- 6.                    
+                        let rect = new Rect(x, y, width, boxHeight)
+                        drawingContext.DrawRectangle (brush, null, rect)       
+                  
 
-    type ScrollBarEnhancer(ed:TextEditor, errs:ErrorHighlighter) = 
+    type ScrollBarEnhancer(ed:TextEditor, state:InteractionState, errs:ErrorHighlighter) = 
     
         let vertScrollBar : ScrollBar =
             ed.ApplyTemplate ()  |> ignore                 
             let scrollViewer = ed.Template.FindName ("PART_ScrollViewer", ed) :?> ScrollViewer
             scrollViewer.ApplyTemplate ()|> ignore
             let vScrollBar = scrollViewer.Template.FindName ("PART_VerticalScrollBar", scrollViewer) :?> ScrollBar        
-            if isNull vScrollBar then 
-                failwithf $"scrollViewer.Template.FindName (\"PART_VerticalScrollBar\")  is null"
+            if isNull vScrollBar then failwithf $"scrollViewer.Template.FindName (\"PART_VerticalScrollBar\")  is null" // never happens
             vScrollBar
         
         let mutable adorner: ScrollbarAdorner = null
@@ -97,26 +111,28 @@ module MagicScrollbar =
         let setAdorner() = 
             if isNull adorner then                
                 let track =  vertScrollBar.Template.FindName ("PART_Track", vertScrollBar) :?> Track    
-                if notNull track then    
-                    let trackGrid = VisualTreeHelper.GetParent (track) :?> Grid       
-                    let layer = AdornerLayer.GetAdornerLayer (trackGrid) 
-                    adorner <- new ScrollbarAdorner(ed, errs, trackGrid)
+                if notNull track then 
+                    //let trackGrid = VisualTreeHelper.GetParent (track) :?> Grid // sharp develop uses this for the adorner layer    
+                    let layer = AdornerLayer.GetAdornerLayer (track) 
+                    adorner <- new ScrollbarAdorner(ed,state, errs, track)
                     layer.Add (adorner)                    
 
         
         do 
             setAdorner()
-            if notNull adorner then adorner.InvalidateVisual()  
+            if notNull adorner then 
+                adorner.IsTrackShowing <- true
+                adorner.InvalidateVisual()  
 
             vertScrollBar.IsVisibleChanged.Add (fun _ -> // when the text is small no scrollbar is visible.
                 //eprintfn $"vScrollBar.VisibleChanged: {vertScrollBar.IsVisible}" // this event even happens while normal scrolling ! why ?
                 if vertScrollBar.IsVisible then 
                     setAdorner()
                     if notNull adorner then
-                        adorner.IsActive <- true
+                        adorner.IsTrackShowing <- true
                         adorner.InvalidateVisual()            
                 elif notNull adorner then
-                    adorner.IsActive <- false
+                    adorner.IsTrackShowing <- false
                     adorner.InvalidateVisual()
             )
             
